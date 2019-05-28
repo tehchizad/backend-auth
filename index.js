56d11ee1da44347e6e42e63c63aab86eedd1f484
unknown <cinfanger@taos.com>
2019-05-27 17:20:22 -0700

    auth init

--- index.js
+++ index.js
@@ -7,44 +7,8 @@ const authRoutes = require('./routes/auth')
 
 const PORT = 5150
 
+app.use(express.json())
 app.use('/api/user', authRoutes)
-
-// Firestore admin init
-const admin = require('firebase-admin')
-const serviceAccount = require('./.env/codettastone-auth.json')
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-})
-
-const db = admin.firestore()
-
-function addQuote() {
-  db.collection('quotes')
-    .doc('learntocode')
-    .create({
-      quote: '#learntocode'
-    })
-    .then(() => console.log('Write successful'))
-}
-
-function readQuotes() {
-  db.collection('quotes')
-    .doc('hashtags')
-    .get()
-    .then(doc => {
-      if (!doc.exists) {
-        console.log('Document does not exist')
-      }
-      console.log('Complete:', doc._fieldsProto)
-    })
-    .catch(err => {
-      console.log('Error:', err)
-      process.exit()
-    })
-}
-
-readQuotes()
 
 // Default listener
 app.listen(PORT, () => console.log(`Serving on port ${PORT}`))
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,51 @@
 const router = require('express').Router()
 
-router.get('/register', (req, res) => {
-  res.send('Registration')
+// Firestore admin init
+const admin = require('firebase-admin')
+const serviceAccount = require('./.env/codettastone-auth.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
+
+const db = admin.firestore()
+
+function addQuote() {
+  db.collection('quotes')
+    .doc('learntocode')
+    .create({
+      quote: '#learntocode'
+    })
+    .then(() => console.log('Write successful'))
+}
+
+function readQuotes() {
+  db.collection('quotes')
+    .doc('hashtags')
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('Document does not exist')
+      }
+      console.log('Complete:', doc._fieldsProto)
+    })
+    .catch(err => {
+      console.log('Error:', err)
+      process.exit()
+    })
+}
+
+router.post('/register', (req, res) => {
+  let user = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }
+  try {
+    res.status(200).send(`${user.email} registered!`)
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 router.get('/login', (req, res) => {
